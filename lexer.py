01f2aee89b11623426291f33cdb2557a8b1d031e
Preston <prestonip8@gmail.com>
2022-12-09 22:35:32 -0500

    Add comments to lexer code

--- lexer.py
+++ lexer.py
@@ -1,6 +1,6 @@
 ####################### TOKENS #######################
 
-
+# token structure
 class Token():
     def __init__(self, type_, code, value):
         self.type = type_
@@ -11,11 +11,13 @@ class Token():
         if self.value: return f'{self.type}: {self.value}'
         return f'{self.type}'
 
-real_literal = 0 #fractional number
-natural_literal = 1     #whole numbers and 0
+# token chart w/ corresponding codes
+
+real_literal = 0 
+natural_literal = 1
 bool_literal = 2
-char_literal = 3 #single ascii character including escape character (java rules)
-string_literal = 4 # any number of ascii characters including escape characters
+char_literal = 3 
+string_literal = 4 
 
 
 assign_op           = 10
@@ -67,6 +69,7 @@ STOP = 99
 
 ####################### LEXER #######################
 
+# lexer with all the lex functions
 class Lexer:
     def __init__(self, text):
         self.text = text
@@ -82,94 +85,119 @@ class Lexer:
         tokens = []
         while self.curr_char != None:
 
-            # real and natural literals
+            # look for numbers
             if self.curr_char.isnumeric(): 
                 num = ""
+                # get more numbers
                 while self.curr_char.isnumeric():
                     num += self.curr_char
                     self.advance()
+                    # if you get a division operator, add it to the current number
                     if self.curr_char == "/":
                         num += self.curr_char
                         self.advance()
+                # if our number has a division op, its a real
                 if '/' in num:
                     tokens.append(Token('real_literal', real_literal, num))
+                # if it doesn't, its a natural
                 else:
                     tokens.append(Token('natural_literal', natural_literal, num))
 
-            # char literals
+            # look for single quote
             elif self.curr_char == "\'" :
                 char_char = ''
                 self.advance()
+                # if no ascii char has been added, output the empty char
                 if self.curr_char == "\'":
                     tokens.append(Token('char_literal', char_literal, char_char))
                     self.advance()
+                # if we get an ascii char that isn't escape...
                 elif self.curr_char.isascii() and self.curr_char != '\\' and self.curr_char != '\'':
                     char_char += self.curr_char
                     self.advance()
+                    # look for another single quote, and output the char
                     if self.curr_char == "\'":
                         tokens.append(Token('char_literal', char_literal, char_char))
                         self.advance()
                     else:   return self.lexError('Invalid character literal format')
+                # if we get escape char...
                 elif self.curr_char == '\\':
                     char_char += self.curr_char
                     self.advance()
+                    # and the next character is one of these...
                     if self.curr_char == 't' or self.curr_char == 'b' or self.curr_char == 'n' or self.curr_char == 'r' or self.curr_char == 'f' or self.curr_char == '\'' or self.curr_char == '\"':
                             char_char += self.curr_char
                             self.advance()
+                            # look for a single quote and output char
                             if self.curr_char == '\'':
                                 tokens.append(Token('char_literal', char_literal, char_char))
                                 self.advance()
+                    # if it's one of these
                     elif self.curr_char == '\\':
                         self.advance()
+                        # look for a single quote and output char
                         if self.curr_char == '\'':
                             tokens.append(Token('char_literal', char_literal, char_char))
                             self.advance()
                     else: return self.lexError('Invalid escape sequence format')
                 else:   return self.lexError('Invalid character literal format')
 
-            # string literals
+            # look for double quote
             elif self.curr_char == "\"" :
                 str_str = ""
                 self.advance()
+                # while we're reading ascii chars...
                 while self.curr_char.isascii() and self.curr_char != "\"":
+                    # if we get escape char...
                     if self.curr_char == "\\":
                         self.advance()
+                        # look for one of these
                         if self.curr_char == 't' or self.curr_char == 'b' or self.curr_char == 'n' or self.curr_char == 'r' or self.curr_char == 'f' or self.curr_char == '\'' or self.curr_char == '\"' or self.curr_char == '\\':
                                 self.advance()
                         else: return self.lexError('Invalid escape sequence format')
+                    # if not, just keep adding ascii to the string
                     else: 
                         str_str += self.curr_char
                         self.advance() 
+                # if we get double quote, output the whole string
                 if self.curr_char == "\"":
                     tokens.append(Token('string_literal', string_literal, str_str))
                     self.advance()
                 else:   return self.lexError('Invalid string literal format')
 
-            # keywords
+            # keywords can be letters and underscores
             elif self.curr_char.isalpha() or self.curr_char == "_":
                 str_str = ""
+                # so while we're reading those, add them to the keyword...
                 while self.curr_char != None and (self.curr_char.isalpha() or self.curr_char == "_"):
                     str_str += self.curr_char 
                     self.advance()
+                # (very janky function calling recognition ahead. please forgive the crusty nature of the following code. thank you.) 
+                # if we get a left paren after out chars...
                 if self.curr_char == "(":
                     self.advance()
+                    # and the next char is a '$'...
                     if self.curr_char == "$":
                         self.advance()
+                        # look for param name
                         if self.curr_char.isalpha() or self.curr_char == "_":
                             str_str = ""
                             while self.curr_char != None and (self.curr_char.isalpha() or self.curr_char == "_"):
                                 str_str += self.curr_char 
                                 self.advance()
+                            # once param name is typed, if we get right paren, and the param we just found isn't a keyword, output the function call, and the parameter...
                             if self.curr_char == ')' and str_str != "try" and str_str != "catch" and str_str != "loop" and str_str != "word" and str_str != "num" and str_str != "char" and str_str != "frac" and str_str != "bool" and str_str != "true" and str_str != "cap" and str_str != "START" and str_str != "STOP":
                                 tokens.append(Token('function_call', function_call, None))
                                 tokens.append(Token('param_dec', param_dec, None))
                                 self.advance()
                             else: return self.lexError("invalid function call")
                         else: return self.lexError("invalid function call")
+                    # and if we get a right paren with no parameters inside, return just the function call
                     elif self.curr_char == ")":
                         tokens.append(Token('function_call', function_call, None))
                         self.advance()
                     else: return self.lexError("invalid function call")
+                # keywords for various stuff:
                 elif str_str == "try":        tokens.append(Token('if_', if_, None)) # selection
                 elif str_str == "catch":    tokens.append(Token('else_', else_, None)) # selection
                 elif str_str == "loop":     tokens.append(Token('loop', loop, None)) # loop
@@ -182,11 +210,13 @@ class Lexer:
                 elif str_str == "cap":      tokens.append(Token('bool_literal', bool_literal, 'false')) # bool_lit false
                 elif str_str == "START":    tokens.append(Token('START', START, None))
                 elif str_str == "STOP":    tokens.append(Token('STOP', STOP, None))
+                # if it's none of any of that, its an variable identifier
                 else:                       
                     tokens.append(Token('ident', ident, None))
 
-            # all other tokens conditions
-            else:    
+            # not keywords or idents, not literals, so special characters.
+            else:
+                # all special characters and character patterns
                 match self.curr_char:
                     case '#':
                         while self.curr_char != '\n' and self.curr_char != None:
@@ -246,13 +276,13 @@ class Lexer:
                     case ',': tokens.append(Token('comma', comma, None)), self.advance()
                     case '[': tokens.append(Token('left_brace', left_brace, None)), self.advance()
                     case ']': tokens.append(Token('right_brace', right_brace, None)), self.advance()
-                    # case '$': tokens.append(Token(param_dec), self.advance()
                     case ' ': self.advance()
                     case '\t': self.advance()
                     case '\n': self.advance()
-                    case _: return self.lexError(self.curr_char)
+                    case _: return self.lexError("ion know, its one of these")
         return tokens
         
+    # error function that takes a hand-typed message from yours truly :)
     def lexError(self, invalid_char):
         self.invalid_char = invalid_char
         return f'Lexical Error: {invalid_char}'
@@ -260,7 +290,7 @@ class Lexer:
 
 
 ####################### RUNNER #######################
-
+# run this bih
 def run(text):
     lexer = Lexer(text)
     tokens = lexer.tokenize()
